4be739dc6bcf197148677914486110040db4f990
Eunjun1 <ibbuni8486@gmail.com>
2025-05-02 14:33:41 +0900

    main 프로젝트 업로드

---
+++ kicksy/lib/view/hq/hq_main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HqMain extends StatefulWidget {
+  const HqMain({super.key});
+
+  @override
+  State<HqMain> createState() => _HqMainState();
+}
+
+class _HqMainState extends State<HqMain> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
